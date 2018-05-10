aac4444a6605f96a9f91bbe166a59fd1e5f1b560
Simon Cain <rudenik@gmail.com>
2018-05-10 15:23:01 -0400

    put all the images on the page. added the input from the field.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-topic = ["Tom Cruise", "Kurt Russell", "Michael Keaton", "Jack Nicholson", "Sharon Stone", 
+topics = ["Tom Cruise", "Kurt Russell", "Michael Keaton", "Jack Nicholson", "Sharon Stone", 
         "Chrstopher Walken", "Harrison Ford", "Lea Thompson", "Geena Davis", "Goldie Hawn", 
         "Sigourney Weaver", "Jennifer Jason Leigh", "Jennifer Connelly", "Kim Basinger",
          "Phoebe Cates", "Kathleen Turner"];
@@ -7,15 +7,15 @@ cities = ["fill these in later with both cityname and state/province/region"];
 
 
 var giphyAPIKey = "19C8uwZhdiB9aK3J93kbQ6ph99HN1tc4";
-var gifphyQueryURL = "https://api.giphy.com/v1/gifs/search?q=" // + "&api_key=" + giphyAPIKey;
-var queryTerm=""
+var gifphyQueryURL = "https://api.giphy.com/v1/gifs/search?" ;// + "&api_key=" + giphyAPIKey;
+var queryTerm="";
 // $.ajax({
 //     url: queryURL,
 //     method: "GET"
 // }).then(function(resp){
 //     console.log(resp)
 // })
-drawButtons(topic);
+drawButtons(topics);
 
 function drawButtons(array){
     $("#buttonrow").empty();
@@ -29,6 +29,38 @@ function drawButtons(array){
 }
 function setQueryTerm(term){
     queryTerm=term;
+}
+function makeRequest(apikey, queryURL, term){
+    $("#results-col").empty();
+    $.ajax({
+        url: queryURL+"api_key="+apikey+"&q="+term+"&limit=10&offset=0&lang=en",//queryURL+term+"&api_key="+apikey+"&limit=10&lang=en&offset=0&rating=Y",
+        method: "GET"
+    }).then(function(resp){
+        console.log(resp["data"]);
+        console.log(resp);
+
+    for (elements in resp["data"]){
+        var cardDiv = $("<div>");
+        cardDiv.addClass("card");
+        cardDiv.attr("style", "max-width: 18rem").attr("data-animated", resp["data"][elements]["images"].fixed_width);
+        var cardBody = $("<div>");
+        cardBody.addClass("card-body").attr("id", "clickToGif");
+        var cardText = $("<p>");
+        cardText.addClass("card-text");
+        cardTextHTML = "<b>Rating: </b>" + resp["data"][elements].rating;
+        cardTextHTML += "<br><b>Title: </b>" + resp["data"][elements].title;
+        cardText.html(cardTextHTML);
+        console.log(resp["data"][elements]["images"].fixed_width["url"]);
+        var imageToAdd = $("<img>")
+        imageToAdd.attr("src", resp["data"][elements]["images"].fixed_width_still["url"])
+        imageToAdd.addClass("card-img-bottom");
+        cardBody.append(cardText);
+        cardBody.append(imageToAdd);
+        cardDiv.append(cardBody)
+        cardDiv.appendTo($("#results-col"));
+        // imageToAdd.appendTo($("#results-col"));
+    }
+})
 }
 $(".btn-group").on("click", "input", function(){
     // $(".btn").button('toggle');
@@ -66,4 +98,18 @@ $("#buttonrow").on("click", "#querybutton", function(){
     var queryTerm = encodeURI($(this).text());
     // console.log(queryTerm);
     setQueryTerm(queryTerm);
+    // console.log(giphyAPIKey+ " queryURL: " + gifphyQueryURL +" term: " +queryTerm)
+    makeRequest(giphyAPIKey, gifphyQueryURL, queryTerm);
+
+})
+$("#add-topic").on("click", function(event){
+    event.preventDefault();
+    var topic = $("#topic-input").val().trim();
+    console.log("Added Topic: " + topic);
+    topics.push(topic);
+    drawButtons(topics);
+    $("#add-topic").val() = "";
+})
+$("div#clickToGif.card-body").on("click", function(){
+    console.log($(this));//not registering.
 })
